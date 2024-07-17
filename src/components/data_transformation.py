6f7230fcde01469c987fe3076511b02ae7ed53de
Mohd Mudassir Ansari <mudassirimranansari@gmail.com>
2024-07-18 00:37:16 +0530

    Data Transformation Class started

---
+++ src/components/data_transformation.py
@@ -0,0 +1,29 @@
+import os
+import sys
+from dataclasses import dataclass
+
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from imblearn.combine import SMOTETomek
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import RobustScaler, FunctionTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+
+from src.exception import CustomException
+from src.logger import logging
+from src.utils import save_object
+
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path = os.path.join("artifacts", "preprocessor.pkl")
+
+
+class DataTransformation:
+    def __init__(self) -> None:
+        self.data_tranformation_config = DataTransformationConfig()
+
+    def get_data_transfer_object(self):
+        pass
